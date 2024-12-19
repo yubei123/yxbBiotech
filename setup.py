20020d13e415465f70e83d5aaa1839457dc96a8b
yubei <yubei@localhost.localdomain>
2024-12-19 15:57:09 +0800

    删除部分文件

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize('/data/yubei/Biotech/app/menu/__init__.py'))
+setup(ext_modules=cythonize('/data/yubei/Biotech/app/user/__init__.py'))
